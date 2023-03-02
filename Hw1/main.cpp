eead91c7670502e4e2a3690bf6ef53a39ae4187d
niomoo <jennyliu.lyh@gmail.com>
2023-03-02 11:48:53 +0800

    [Hw1]Add comment to the program file

--- Hw1/main.cpp
+++ Hw1/main.cpp
@@ -4,9 +4,12 @@
 #include <iostream>
 using namespace std;
 
+/**
+ * @brief class for passenger
+ */
 class Passenger {
     string name;                    // name of the passenger
-    string phone;             // phone of the passenger
+    string phone;                   // phone of the passenger
     vector<int> tickets;            // tickets booked by the passenger
     
     public:
@@ -16,15 +19,27 @@ class Passenger {
         string getName() const;     // get the name of the passenger
 };
 
+/**
+ * @brief Passenger constructor
+ * @param nameString passenger name
+ * @param phoneNumber passenger phone number
+ */
 Passenger::Passenger(string nameString, string phoneNumber) {
     name = nameString;
     phone = phoneNumber;
 }
 
+/**
+ * @brief Add ticket to the list
+ * @param bus route number
+ */
 void Passenger::addTicket(int bus) {
     tickets.push_back(bus);
 }
 
+/**
+ * @brief Print the tickets of the passenger
+ */
 void Passenger::showBooking() {
     if(tickets.size() == 0) {
         cout << "Passenger " << name << " has no tickets" << endl;
@@ -37,10 +52,17 @@ void Passenger::showBooking() {
     }
 }
 
+/**
+ * @brief Get the name from the passenger
+ * @return the name of the passenger
+ */
 string Passenger::getName() const {
     return name;
 }
 
+/**
+ * @brief class for bus
+ */
 class Bus {
     int route;                      // route number
     int passengerCount;             // number of passengers
@@ -53,12 +75,20 @@ class Bus {
         int getRoute() const;        // get the route of the bus
 };
 
+/**
+ * @brief Bus constructor
+ * @param routeNumber route number for the bus
+ */
 Bus::Bus(int routeNumber)
 {
     route = routeNumber;
     passengerCount = 0;
 };
 
+/**
+ * @brief Add passenger to the bus
+ * @param name passenger name for the ticket
+ */
 bool Bus::addPassenger(string name) {
     if (passengerCount < 10) {
         passengerList.push_back(name);
@@ -68,6 +98,9 @@ bool Bus::addPassenger(string name) {
     return false;
 }
 
+/**
+ * @brief Print the passengers of the bus
+ */
 void Bus::showPassenger() {
     cout << "Passenger list of the bus " << route << ": ";
     for(auto passenger : passengerList) {
@@ -76,39 +109,62 @@ void Bus::showPassenger() {
     cout << endl;
 }
 
+/**
+ * @brief Get the route number of the bus
+ * @return the route number of the bus
+ */
 int Bus::getRoute() const {
     return route;
 }
 
+/**
+ * @brief class for the booking system
+ */
 class System {
-    vector<Bus> busList;                  
-    vector<Passenger> passengerList;
+    vector<Bus> busList;                    // list of bus
+    vector<Passenger> passengerList;        // list of passenger
 
     public:
-        createBus(int);                    // create a new bus route
-        createPassenger(string, string);   // create a new passenger
-        bookTicket(string, int);           // create booking record
-        showBusBooking(int);        
-        showPassengerTicket(string);
+        createBus(int);                     // create a new bus route
+        createPassenger(string, string);    // create a new passenger
+        bookTicket(string, int);            // create booking record
+        showBusBooking(int);                // show the booking record
+        showPassengerTicket(string);        // show the passenger tickets
 };
 
+/**
+ * @brief Create a bus class
+ * @param route route number of the bus
+ */
 System::createBus(int route) {
     Bus bus(route);
     busList.push_back(bus);
 }
 
+/**
+ * @brief Create a passenger class
+ * @param name name of the passenger
+ * @param phone phone number of the passenger 
+ */
 System::createPassenger(string name, string phone) {
     Passenger passenger(name, phone);
     passengerList.push_back(passenger);
 }
 
+/**
+ * @brief Create a ticket booking record
+ * @param name name of the passenger
+ * @param route route number of the bus
+ */
 System::bookTicket(string name, int route) {
     for(auto & passenger: passengerList) {
         if(passenger.getName() == name) {
             for(auto & bus: busList) {
                 if(bus.getRoute() == route) {
-                    if(bus.addPassenger(passenger.getName())) {
+                    if(bus.addPassenger(passenger.getName())) {        // check if the bus seat is available
                         passenger.addTicket(bus.getRoute());
+                    } else {
+                        cout << "Ticket for Bus " << bus.getRoute() << " is not available." << endl;
                     }
                 }
             }
@@ -116,6 +172,10 @@ System::bookTicket(string name, int route) {
     }
 }
 
+/**
+ * @brief Print the passenger of the bus
+ * @param route route number of the bus
+ */
 System::showBusBooking(int route) {
     for(auto bus: busList) {
         if(bus.getRoute() == route) {
@@ -124,6 +184,10 @@ System::showBusBooking(int route) {
     }
 }
 
+/**
+ * @brief Print the ticket booked by the passenger
+ * @param name name of the passenger
+ */
 System::showPassengerTicket(string name) {
     for(auto passenger: passengerList) {
         if(passenger.getName() == name) {
@@ -133,7 +197,9 @@ System::showPassengerTicket(string name) {
 }
 
 int main() {
+
     System system;
+
     system.createBus(101);
     system.createBus(202);
 
